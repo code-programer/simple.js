d15ae01e694f6eebfbe92c04a429fe71f5340a8a
Code Programer <93539221+code-programer@users.noreply.github.com>
2023-02-12 18:47:07 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,3 +2,26 @@ Math.median = function(...e){
   var arr = e;
   return eval(arr.toString().replaceAll(",", " + ")) / arr.length;
 }
+Math.mcm = function(...e){
+  var res = [];
+  for(i in e){
+    var t = e[i];
+    res[i] = [];
+    var n = 2;
+    while(n<=100){
+      if(eval(t % n) == 0){
+        res[i].push(n)
+      }
+      n = n + 1;
+    }
+  }
+  for(pos in res[0]){
+    var p = res[0][pos];
+    var fi = 0;
+    for(list in res){
+      if(res[list].includes(p)){
+        fi += 1;
+      }
+    }
+  }
+}
